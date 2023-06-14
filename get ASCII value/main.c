06a24550f0c527b55783886b6558389da0cd97dd
nabhishekn <abhisheknn1994@gmail.com>
2023-06-14 22:45:52 +0530

    get ASCII value of character

---
+++ get ASCII value/main.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+
+To get ASCII value of a character
+
+*******************************************************************************/
+# include <stdio.h>
+int main( )
+{
+    char ch;
+    printf("Enter the character: ");
+    scanf("%c", &ch);
+    printf("%d", ch);
+return 0 ;
+}
