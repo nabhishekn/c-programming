525813f6800e276241544bc76d859bc88a83c743
nabhishekn <abhisheknn1994@gmail.com>
2023-06-16 23:17:15 +0530

    swap 2 bits of hex

---
+++ practice_9/main.c
@@ -0,0 +1,21 @@
+/*Program to swap 1st and 2nd bit of hexadecimal value stored in data variable*/
+
+#include <stdio.h>
+
+int main(){
+    unsigned char data = 0xA; //Assigning hexadecimal value
+
+    //get 1st bit from data
+    unsigned char bit_1 = (data >> 1) & 1;
+
+    //get 2nd bit from data
+    unsigned char bit_2 = (data >> 2) & 1;
+
+    //get xor of bit 1 and bit 2
+    unsigned char xor_of_bit = bit_1 ^ bit_2;
+
+    printf("After swapping the bits, data value is: %2X", data ^ (xor_of_bit << 1 | xor_of_bit << 2));
+
+    return 0; 
+
+}
