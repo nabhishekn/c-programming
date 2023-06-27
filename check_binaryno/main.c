d4996a953880bd66f5829113c2cf2dd405a3aeb9
nabhishekn <abhisheknn1994@gmail.com>
2023-06-27 23:31:54 +0530

    check number is binary or not

---
+++ check_binaryno/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+  int j, num;
+  printf("Please enter a number :");
+  scanf("%d", & num);
+  while (num > 0) {
+    j = num % 10;
+    if (j != 0 && j != 1) {
+      printf("Given number is not in binary");
+      break;
+    }
+    num = num / 10;
+    if (num == 0) {
+      printf("Given number is in binary");
+    }
+  }
+  getch();
+}
